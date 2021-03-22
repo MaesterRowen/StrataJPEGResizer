111b09680e1c3110aa246ad72f93860e024d3545
Jeffrey Iveron <maesterrowen@gmail.com>
2021-03-21 19:01:23 -0700

    Project Setup
    Project Setup

---
+++ StrataJPEGResizer/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.IO;
+
+namespace StrataJPEGResizer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void fileDropStackPanel_Drop(object sender, DragEventArgs e)
+        {
+            if( e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+                string firstFileName = Path.GetFileName(files[0]);
+
+                fileNameLabel.Content = firstFileName;
+            }
+        }
+    }
+}
