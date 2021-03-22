21a2f7907acb7e35024cfa96a90d6a3bfe66b72c
Jeffrey Iveron <maesterrowen@gmail.com>
2021-03-21 20:10:09 -0700

    added Magick library

--- StrataJPEGResizer/MainWindow.xaml.cs
+++ StrataJPEGResizer/MainWindow.xaml.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.IO;
+using ImageMagick;
 
 namespace StrataJPEGResizer
 {
@@ -20,13 +21,49 @@ namespace StrataJPEGResizer
 
         private void fileDropStackPanel_Drop(object sender, DragEventArgs e)
         {
-            if( e.Data.GetDataPresent(DataFormats.FileDrop))
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
             {
                 string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
                 string firstFileName = Path.GetFileName(files[0]);
 
+
+                string filePath = Path.GetDirectoryName(files[0]);
+                string fileNameWithoutExtension = Path.GetFileNameWithoutExtension(files[0]);
+
+                string newPath = filePath + "\\" + fileNameWithoutExtension + ".jpg";
+
                 fileNameLabel.Content = firstFileName;
+
+
+                using (var image = new MagickImage())
+                {
+                    image.Read(files[0]);
+                    image.Progress += Image_Progress;
+                    
+                    image.Format = MagickFormat.Jpeg;
+                    image.Quality = 75;
+
+                    image.Resize(new Percentage(50.0));
+                    //image.Write(newPath);
+                }
+
+                ////using (var image = new MagickImage(files[0]) )
+                ////{
+                //    image.Format = MagickFormat.Jpeg;
+                //    image.Quality = 75;
+                //    image.Resize(new Percentage(50.0));
+
+                //    image.Write(newPath);
+
+                //}
             }
         }
+
+        private void Image_Progress(object sender, ProgressEventArgs e)
+        {
+            Console.WriteLine(e.Progress.ToString());
+
+        }
+
     }
 }
